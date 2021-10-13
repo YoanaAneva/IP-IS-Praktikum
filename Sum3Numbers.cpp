7b708d6a275664464c39888a2823471a071fa791
YoanaAneva <92211354+YoanaAneva@users.noreply.github.com>
2021-10-13 21:21:40 +0300

    Add files via upload

---
+++ Sum3Numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int firstNumber, secondNumber, thirdNumber;
+
+    cout << "fisrtNumber: ";
+    cin >> firstNumber;
+    cout << "secondNumber: ";
+    cin >> secondNumber;
+    cout << "thirdNumber: ";
+    cin >> thirdNumber;
+    int result = firstNumber + secondNumber + thirdNumber;
+    cout << "result: ";
+    cout << result << endl;
+    return 0;
+}
+
---
+++ ValueExchange.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number1, number2;
+    cout << "firstNumber: ";
+    cin >> number1;
+    cout << "secondNumber: ";
+    cin >> number2;
+    cout << "\n";
+    cout << "firstNumber: " << number2 << endl;
+    cout << "secondNumber: " << number1 << endl;
+    return 0;
+}
